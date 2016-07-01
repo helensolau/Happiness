941a0cee9bc498f3e129adeefa8d4aa40c764548
Helen Lau <hlau@lyft.com>
2016-06-30 21:40:16 -0700

    Initial Commit

---
+++ Happiness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happiness
+//
+//  Created by Helen Lau on 6/30/16.
+//  Copyright © 2016 Helen Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
